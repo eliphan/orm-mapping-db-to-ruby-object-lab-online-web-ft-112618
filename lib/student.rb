17e111235057cd0f5814a8f9efb68b31e198621a
Elise Phan <aliceconnic@gmail.com>
2019-01-11 02:08:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,8 @@ class Student
     WHERE grade = 10
     LIMIT #{x}
     SQL
-    DB[:conn].execute(sql)
+       DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
   
   def self.first_student_in_grade_10
